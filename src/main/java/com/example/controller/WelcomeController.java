b5cfb827cc358603db165935a074d691b6d69225
vinod swarna <swrnvinod40@gmail.com>
2017-02-13 20:10:11 -0600

    gearhost

--- src/main/java/com/example/controller/WelcomeController.java
+++ src/main/java/com/example/controller/WelcomeController.java
@@ -2,7 +2,9 @@ package com.example.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +14,14 @@ public class WelcomeController {
 	// inject via application.properties
 	private String message = "Hello World";
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "welcome";
+		String taxRate = jdbcTemplate.queryForObject("select tax_rate from tax.unitedstates_sales", String.class);
+		model.put("message", taxRate);
+		return "filledform";
 	}
 	
 	
